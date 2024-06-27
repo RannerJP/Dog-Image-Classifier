54afa2a2ac6e092efc39c1c9c2d56fe870cd2738
Jose Pavon <RannerJP@gmail.com>
2024-06-27 14:47:56 -0700

    test with valid file and model

--- tests/test_image_classifier.py
+++ tests/test_image_classifier.py
@@ -8,10 +8,10 @@ from keras.models import load_model
 class TestImageClassifier(unittest.TestCase):
     def test_valid_model_and_image(self):
         model = load_model(os.path.join('models', 'DogClassification.h5'))
-        classi = ImageClassifier(model)
-        image = Image.open("assets/Valid_Image.jpg")
+        classifier = ImageClassifier(model)
+        image = Image.open(os.path.join('assets', 'Valid_Image.jpg'))
         image = image.resize((256,256))
-        predicdition = classi.get_prediction()
-        self.assertIn(predicdition, [-1] + list(range(1,11)))
+        prediction = classifier.get_prediction(image)
+        self.assertIn(prediction, [-1] + list(range(1,11)))
 
         
