8e79aa80962c365b2ea9c542d206b84c6a69c7ed
Jose Pavon <RannerJP@gmail.com>
2024-06-26 23:31:49 -0700

    added test for a valid model and image

--- tests/test_image_classifier.py
+++ tests/test_image_classifier.py
@@ -1,4 +1,17 @@
+import os
 import unittest
-from unittest import patch, MagicMock
-import tkinter as tk
 from image_classifier import ImageClassifier
+from PIL import Image
+from keras.models import load_model
+
+
+class TestImageClassifier(unittest.TestCase):
+    def test_valid_model_and_image(self):
+        model = load_model(os.path.join('models', 'DogClassification.h5'))
+        classifier = ImageClassifier(model)
+        image = Image.open("assets/Valid_Image.jpg")
+        image = image.resize((256,256))
+        predicdition = classifier.get_prediction()
+        self.assertIn(predicdition, [-1] + list(range(1,11)))
+
+        
