1c2ff1b41d72cf0f54e21680a5504fec1c70f6cd
Jose Pavon <RannerJP@gmail.com>
2024-06-28 14:44:56 -0700

    make testing work on github actions hopefully

--- tests/test_image_classifier.py
+++ tests/test_image_classifier.py
@@ -3,11 +3,16 @@ import unittest
 from dogclassification.image_classifier import ImageClassifier
 from PIL import Image
 from keras.models import load_model
+import urllib.request
 
 
 class TestImageClassifier(unittest.TestCase):
     def test_valid_model_and_image(self):
-        model = load_model(os.path.join('models', 'DogClassification.h5'))
+        try:
+            model = load_model(os.path.join('models', 'DogClassification.h5'))
+        except OSError:
+            file = urllib.request.urlretrieve("https://github.com/RannerJP/Dog-Image-Classifier/raw/main/models/DogClassification.h5?download=", ".h5")
+            model = load_model(file[0])
         classifier = ImageClassifier(model)
         image = Image.open(os.path.join('assets', 'Valid_Image.jpg'))
         image = image.resize((256,256))
