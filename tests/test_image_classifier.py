2cfc6138b3a548964577cfe10b3322721be95442
Jose Pavon <RannerJP@gmail.com>
2024-06-29 02:18:41 -0700

    added setup to load model and classifier added test classes for testing if model is invalid, if model setting is invalid, if image is invalid type, and if image is invalid size

--- tests/test_image_classifier.py
+++ tests/test_image_classifier.py
@@ -7,16 +7,39 @@ import urllib.request
 
 
 class TestImageClassifier(unittest.TestCase):
+    def setUp(self):    
+            try:
+                self.valid_model = load_model(os.path.join('models', 'DogClassification.h5'))
+            except OSError:
+                file = urllib.request.urlretrieve("https://github.com/RannerJP/Dog-Image-Classifier/raw/main/models/DogClassification.h5?download=", ".h5")
+                self.valid_model = load_model(file[0])
+            self.classifier = ImageClassifier(self.valid_model)
+    
     def test_valid_model_and_image(self):
-        try:
-            model = load_model(os.path.join('models', 'DogClassification.h5'))
-        except OSError:
-            file = urllib.request.urlretrieve("https://github.com/RannerJP/Dog-Image-Classifier/raw/main/models/DogClassification.h5?download=", ".h5")
-            model = load_model(file[0])
-        classifier = ImageClassifier(model)
+        self.classifier.set_model(self.valid_model)
         image = Image.open(os.path.join('assets', 'Valid_Image.jpg'))
         image = image.resize((256,256))
-        prediction = classifier.get_prediction(image)
+        prediction = self.classifier.get_prediction(image)
         self.assertIn(prediction, [-1] + list(range(1,11)))
+    
+    def test_invalid_model_init(self):
+        with self.assertRaises(TypeError):
+            fail_classifier = ImageClassifier(None)
+    
+    def test_invalid_model_set(self):
+        with self.assertRaises(TypeError):
+            self.classifier.set_model(None)
+    
+    def test_invalid_image_type(self):
+        image = os.path.join('assets', 'Invalid_Image.txt')
+        with self.assertRaises(TypeError):
+            self.classifier.get_prediction(image)    
 
-        
+    def test_invalid_image_size(self):
+        self.classifier.set_model(self.valid_model)
+        image = Image.open(os.path.join('assets', 'Valid_Image.jpg'))
+        prediction = self.classifier.get_prediction(image)
+        self.assertIn(prediction, [-1] + list(range(1,11)))
+    
+    if __name__ == '__main__':
+        unittest.main()
