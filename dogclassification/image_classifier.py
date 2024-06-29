ea8bf4d80a9b01b03d04463563c782ce2374e737
Jose Pavon <RannerJP@gmail.com>
2024-06-29 02:17:36 -0700

    Added check for if model is a correct type Added check for if image is correct type Resizes image if not the correct size

--- dogclassification/image_classifier.py
+++ dogclassification/image_classifier.py
@@ -1,14 +1,23 @@
 import numpy as np
 from tensorflow.keras.models import Model
+from keras.models import Sequential
 from PIL import Image
 class ImageClassifier:
     """
     The ImageClassifier Class handles any predictions made with the model
     """
-    def __init__(self, model: Model):
+    def __init__(self, model: Sequential):
+        if not isinstance(model, Sequential):
+            raise TypeError("The model should be a Keras Sequential model.")
         self.model = model
 
     def get_prediction(self, image: Image):
+        if not isinstance(self.model, Sequential):
+            return -2
+        if not isinstance(image, Image.Image):
+            raise TypeError("user inputted photo is not of type Image.Image")
+        if image.size != (256, 256):
+            image = image.resize((256, 256))
         np_image = np.array(image)
         prediction = self.model.predict(np.expand_dims(np_image/255, 0))
         prediction = prediction.tolist()
@@ -17,4 +26,6 @@ class ImageClassifier:
         return dog_class_predicted
     
     def set_model(self, model_to_set: Model):
+        if not isinstance(model_to_set, Sequential):
+            raise TypeError("The model should be a Keras Sequential model.")
         self.model = model_to_set
