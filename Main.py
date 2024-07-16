c3a2d7ad2ed0a7a0005bcde35cd7f32b36414095
Jose Pavon <RannerJP@gmail.com>
2024-07-16 04:39:55 -0700

    Downloads model if file is not found

--- Main.py
+++ Main.py
@@ -1,11 +1,15 @@
 import os
-import sys
+import urllib.request
 from tkinter import Tk
 from dogclassification.dog_classification_UI import DogClassificationUI
 from keras.models import load_model
 
 if __name__ == '__main__':
-    model = load_model(os.path.join('models', 'DogClassification.h5'))
+    try:
+        model = load_model(os.path.join('models', 'DogClassification.h5'))
+    except OSError:
+        file = urllib.request.urlretrieve("https://github.com/RannerJP/Dog-Image-Classifier/raw/main/models/DogClassification.h5?download=", ".h5")
+        model = load_model(file[0])    
     window = Tk()
     user_interface = DogClassificationUI(window, model)
     user_interface.show_UI()
